730b7ec48b3685d6094acb427ffc2c99eda95d4f
Albert Suarez <albert.suarez.molgo@est.fib.upc.edu>
2015-11-26 08:51:47 +0100

    exercicis 1.1.4.*

--- Qt/S1-IntroQt/Excercici2/lab0.cpp
+++ Qt/S1-IntroQt/Excercici2/lab0.cpp
@@ -21,24 +21,29 @@ int main(int argc, char **argv )
     // Crea un frame
     QFrame F(0, NULL);
 
-    // Crea un contenidor horitzontal
+    // Crea un contenidor horitzontal/vertical
     QHBoxLayout* cH = new QHBoxLayout(&F);
+    //QVBoxLayout* cH = new QVBoxLayout(&F);
 
     // Afegeix una caixa de text
     QLineEdit* le = new QLineEdit(&F);
     cH->addWidget(le);
 
-    // Afegeix un espai (horitzontal, vertical)
+    // Afegeix un espai en el contenidor horitzontal/vertical per la dreta/per abaix. El parametre que ho indica es el primer.
     QSpacerItem *sp = new QSpacerItem(100,20);
     cH->addItem(sp);
 
+    // Creem un layout vertical per tal d'afegir nomes els dos botons en posicio vertical
+    QVBoxLayout* cV = new QVBoxLayout(&F);
+    cH->addItem(cV);
+
     // Afegeix un boto
     QPushButton* ok = new QPushButton("D’acord", &F);
-    cH->addWidget(ok);
+    cV->addWidget(ok);
 
     // Afegeix un altre boto
     QPushButton* surt = new QPushButton("Surt", &F);
-    cH->addWidget(surt);
+    cV->addWidget(surt);
 
     F.show();
     return a.exec();
